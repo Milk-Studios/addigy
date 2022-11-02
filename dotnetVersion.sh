9e193c60139702bfe2ff19488cb2e7652a8dfe47
Marc Kachtan <marckachtan@gmail.com>
2022-11-02 11:55:59 -0400

    referencing /etc/install_location file to simplify the script

--- dotnetVersion.sh
+++ dotnetVersion.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-dotnet5binary="/usr/local/share/dotnet/dotnet"
-dotnet6binary="/usr/local/share/dotnet/x64/dotnet"
+## Returns a list of the installed dotnet runtime versions.
+## Set the Return Type to List
 
-if [ -f "$dotnet5binary" ]; then
-	"$dotnet5binary" --list-runtimes | awk {'print $2'}
-elif [ -f "$dotnet6binary" ]; then
-	"$dotnet6binary" --list-runtimes | awk {'print $2'}
+binary_location=`cat /etc/dotnet/install_location`
+
+if [ -f "$binary_location/dotnet" ]; then
+	"$binary_location/dotnet" --list-runtimes | awk {'print $2'}
 else
 	exit 117
 fi
